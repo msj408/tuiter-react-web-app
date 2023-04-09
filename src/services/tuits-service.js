65dd208114c7615b091f8b4a154253ae8f289339
Manu Singh <manbir410@gmail.com>
2023-04-09 16:47:03 -0700

    assignment 9 with local host

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = 'https://tuiter-node-server-app-yzrq.onrender.com';
+const TUITS_API = 'http://localhost:4000/api/tuits';
 
 
 export const createTuit = async (tuit) => {
